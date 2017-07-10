9a714bae5ad83077e096e74b8f3b8c6afd014fa2
Tom Lagier <tom@gliffy.com>
2017-07-10 08:15:40 -0700

    Change dockerfile to just pull from docker hub - need to do this because the profiles are too large to transfer through git

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-PARENT="$(pwd)"
-PROJECT=${PARENT##*/}
-
-docker rm -f $PROJECT
-docker build -t tomlagier/$PROJECT -f ./scripts/Dockerfile ./
+docker rm -f heapviz
+docker pull tomlagier/heapviz:latest
 HOST_PORT=$(jq -r .hostPort package.json)
-docker run -d -p $HOST_PORT:80 --name $PROJECT tomlagier/$PROJECT
+docker run -d -p $HOST_PORT:80 --name heapviz tomlagier/heapviz
